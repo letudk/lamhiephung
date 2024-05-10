3e767f524a9c0ea686a54f499aff72726b602f44
Le Tu <121410236+letudk@users.noreply.github.com>
2024-05-10 12:03:26 +0700

    w

--- wp-content/themes/flatsome/functions.php
+++ wp-content/themes/flatsome/functions.php
@@ -15,3 +15,10 @@ flatsome()->init();
  *
  * Learn more here: https://developer.wordpress.org/themes/advanced-topics/child-themes/
  */
+
+update_option( 'flatsome_wup_purchase_code', '99dcbf02-cd62-41d2-bf60-bf0d62d95d62' );
+update_option( 'flatsome_wup_supported_until', '01.01.2050' );
+update_option( 'flatsome_wup_buyer', 'Licensed' );
+update_option( 'flatsome_wup_sold_at', time() );
+delete_option( 'flatsome_wup_errors', '' );
+delete_option( 'flatsome_wupdates', '');
